708db0fd54352525ccdcf6aa0904573d8cd2bc9f
86167 <1372942864@qq.com>
2023-09-09 12:23:22 +0800

    mlp全连接

--- utils/model/dnn/mdp_dnn.py
+++ utils/model/dnn/mdp_dnn.py
@@ -107,7 +107,7 @@ def nerual_network():
         y_test = np.array(labels)[test_index]
 
         clf = MLPClassifier(hidden_layer_sizes=(50,20), activation='tanh', solver='sgd', alpha=0.001,
-                            batch_size=20, learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=200,
+                            batch_size=5, learning_rate='constant', learning_rate_init=0.01, power_t=0.5, max_iter=200,
                             shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9,
                             nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9,
                             beta_2=0.999, epsilon=1e-08, n_iter_no_change=10)
