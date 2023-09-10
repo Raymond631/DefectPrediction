482dbc28884e3103e354279567851d0bde5d3921
Raymond <raymondli631@163.com>
2023-09-10 14:29:13 +0800

    SVM模型添加ROC曲线；修复读取最后一列名称不同的bug

--- models/svm/svm.py
+++ models/svm/svm.py
@@ -1,10 +1,10 @@
 import os
 
 import pandas as pd
+from matplotlib import pyplot as plt
 from scipy.io import arff
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
 
 
@@ -12,7 +12,7 @@ def svm(folder_path):
     # 获取目录下的所有ARFF文件
     arff_files = [f for f in os.listdir(folder_path) if f.endswith('.arff')]
     combined_data = pd.DataFrame()
-
+    # 读取数据
     for filename in arff_files:
         file_path = os.path.join(folder_path, filename)
         # 从ARFF文件加载数据
@@ -20,43 +20,45 @@ def svm(folder_path):
         df = pd.DataFrame(data)
         # 将数据添加到合并的数据集中
         combined_data = pd.concat([combined_data, df], ignore_index=True)
-
-    # 使用LabelEncoder将字符串目标变量转换为数值
-    label_encoder = LabelEncoder()
-    combined_data['class'] = label_encoder.fit_transform(combined_data['class'])
-    class_labels = [label.decode('utf-8') for label in label_encoder.classes_]  # 保存原始的枚举类型标签
+    # 将标签列转换为二进制（0和1）
+    combined_data.iloc[:, -1] = combined_data.iloc[:, -1].apply(lambda x: 0 if x == b'buggy' else 1)
 
     # 分割数据为特征 (X) 和目标变量 (y)
     X = combined_data.iloc[:, :-1]
-    y = combined_data['class']
-
+    y = combined_data.iloc[:, -1].astype(int)
     # 将数据分割为训练集和测试集
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # 创建SVM分类器
-    svm_classifier = SVC(kernel='rbf')  # 可以用不同的核函数,如'linear'、'rbf'等
-
+    svm_model = SVC(kernel='rbf', probability=True)  # 可以用不同的核函数,如'linear'、'rbf'等
     # 训练模型
-    svm_classifier.fit(X_train, y_train)
+    svm_model.fit(X_train, y_train)
 
     # 使用模型进行预测
-    y_pred = svm_classifier.predict(X_test)
+    y_pred = svm_model.predict(X_test)
+    y_prob = svm_model.predict_proba(X_test)[:, 1]
 
-    # 评估模型性能
+    # 准确率
     accuracy = accuracy_score(y_test, y_pred)
     print(f'Accuracy: {accuracy:.2f}')
-
-    # 打印分类报告
-    print(classification_report(y_test, y_pred, target_names=class_labels))
+    # 分类报告
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred, target_names=['buggy', 'clean']))
+    # 计算ROC曲线
+    fpr, tpr, thresholds = roc_curve(y_test, y_prob)
+    roc_auc = roc_auc_score(y_test, y_prob)
+    # 绘制ROC曲线
+    plt.figure(figsize=(8, 6))
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc='lower right')
+    plt.show()
 
 
 if __name__ == '__main__':
     svm('../../data/arff/AEEEM')
-    # data_path = "../../data"
-    # # 所有数据集
-    # data_sets = [f for f in os.listdir(data_path)]
-    # # 分别使用各个数据集
-    # for data_set in data_sets:
-    #     print(f'数据集：{data_set}')
-    #     svm(os.path.join(data_path, data_set))
-    #     print('-----------------------------')
