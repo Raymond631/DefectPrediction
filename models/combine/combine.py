633842f1d87f25a1f0eb919af8f14f71439c768f
PRay <2519219483@qq.com>
2023-09-14 01:33:07 +0800

    组合完成

--- models/combine/combine.py
+++ models/combine/combine.py
@@ -1,14 +1,55 @@
-from sklearn.neural_network import MLPClassifier
-
+# 引入模型
+from models.ADTree.adtree import train_adt, test_adt
+from models.Adaboost.adaboost import train_adaboost, test_adaboost
+from models.DT.dt import train_dt, test_dt
+from models.LR.lrtest import train_lr, test_lr
+from models.Naive_Bayes.model_nb import train_nb, test_nb
+from models.XGboost.xg import train_xgboost, test_xgboost
+from models.knn.knn import train_knn, test_knn
+from models.mlp.mlp_nk import train_mlp, test_mlp
+from models.random_forest.mdp_random import train_rf, test_rf
 from models.svm.svm import train_svm, test_svm
 from utils.common import read_arff, data_split, data_standard_scaler, model_evaluation
 
+df = ''
 
-def base_classifier(X_train, y_train, X):
-    # 训练模型
-    train_svm(X_train, y_train)
-    # 预测
-    y_pred, y_prob = test_svm(X)
+
+def base_classifier(X_train, y_train, X, model):
+    global df
+    if model == 'naive_bayes':
+        # 训练模型
+        train_nb(X_train, y_train)
+        # 预测
+        y_pred, y_prob = test_nb(X)
+    elif model == 'svm':
+        train_svm(X_train, y_train)
+        y_pred, y_prob = test_svm(X)
+    elif model == 'ADTree':
+        train_adt(X_train, y_train)
+        y_pred, y_prob = test_adt(X)
+    elif model == 'dt':
+        train_dt(X_train, y_train)
+        y_pred, y_prob = test_dt(X)
+    elif model == 'lr':
+        train_lr(X_train, y_train)
+        y_pred, y_prob = test_lr(X)
+    elif model == 'mlp':
+        train_mlp(X_train, y_train)
+        y_pred, y_prob = test_mlp(X)
+    elif model == 'adaboost':
+        train_adaboost(X_train, y_train)
+        y_pred, y_prob = test_adaboost(X)
+    elif model == 'xgboost':
+        train_xgboost(X_train, y_train)
+        y_pred, y_prob = test_xgboost(X)
+    elif model == 'random_forest':
+        train_rf(X_train, y_train)
+        y_pred, y_prob = test_rf(X)
+    elif model == 'knn':
+        train_knn(X_train, y_train)
+        y_pred, y_prob = test_knn(X)
+    else:
+        y_pred, y_prob = test_knn(X)
 
     # 将预测结果插入数据集
     df.insert(loc=0, column='pred', value=y_pred)
@@ -18,19 +59,61 @@ def base_classifier(X_train, y_train, X):
     return P_train, P_test, q_train, q_test
 
 
-def meta_classifier(X_train, y_train, X_test):
-    clf = MLPClassifier(hidden_layer_sizes=(40, 80, 60, 40, 20, 10, 5, 2, 1), activation='tanh', solver='sgd',
-                        alpha=0.001, batch_size=50, learning_rate='adaptive', learning_rate_init=0.03, power_t=0.5, max_iter=200,
-                        shuffle=True, random_state=42, tol=0.0001, verbose=True, warm_start=True, momentum=0.9,
-                        nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9,
-                        beta_2=0.999, epsilon=1e-08, n_iter_no_change=10)
-
-    for i in range(1):
-        clf.fit(X_train, y_train)
-
-    y_pred = clf.predict(X_test)
-    y_prob = clf.predict_proba(X_test)[:, 1]
+def meta_classifier(X_train, y_train, X_test, model):
+    if model == 'naive_bayes':
+        train_nb(X_train, y_train)
+        y_pred, y_prob = test_nb(X_test)
+    elif model == 'svm':
+        train_svm(X_train, y_train)
+        y_pred, y_prob = test_svm(X_test)
+    elif model == 'ADTree':
+        train_adt(X_train, y_train)
+        y_pred, y_prob = test_adt(X_test)
+    elif model == 'dt':
+        train_dt(X_train, y_train)
+        y_pred, y_prob = test_dt(X_test)
+    elif model == 'lr':
+        train_lr(X_train, y_train)
+        y_pred, y_prob = test_lr(X_test)
+    elif model == 'mlp':
+        train_mlp(X_train, y_train)
+        y_pred, y_prob = test_mlp(X_test)
+    elif model == 'adaboost':
+        train_adaboost(X_train, y_train)
+        y_pred, y_prob = test_adaboost(X_test)
+    elif model == 'xgboost':
+        train_xgboost(X_train, y_train)
+        y_pred, y_prob = test_xgboost(X_test)
+    elif model == 'random_forest':
+        train_rf(X_train, y_train)
+        y_pred, y_prob = test_rf(X_test)
+    elif model == 'knn':
+        train_knn(X_train, y_train)
+        y_pred, y_prob = test_knn(X_test)
+    else:
+        y_pred, y_prob = test_knn(X)
     return y_pred, y_prob
+
+
+def combine_models(folder_path, bug_label, models):
+    global df
+    # 读取arff数据集
+    df = read_arff(folder_path, bug_label)
+    # 特征变量
+    X = df.iloc[:, :-1].values
+    # 将数据分割为训练集和测试集
+    X_train, X_test, y_train, y_test = data_split(df)
+    # 标准化特征数据
+    X_train, X_test = data_standard_scaler(X_train, X_test)
+
+    # 基分类器
+    for i in range(len(models) - 1):
+        X_train, X_test, y_train, y_test = base_classifier(X_train, y_train, X, models[i])
+
+    # 元分类器
+    y_pred, y_prob = meta_classifier(X_train, y_train, X_test, models[-1])
+    # 模型评估
+    model_evaluation(y_test, y_pred, y_prob)
 
 
 if __name__ == '__main__':
--- ui/model/index.py
+++ ui/model/index.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
 
+from ttkbootstrap import Style
+
 from models.ADTree.adtree import ad_tree
 # 引入模型
 from models.Adaboost.adaboost import adaboost
@@ -8,6 +10,7 @@ from models.DT.dt import decision_tree
 from models.LR.lrtest import lr
 from models.Naive_Bayes.model_nb import naive_bayes
 from models.XGboost.xg import xgboost
+from models.combine.combine import combine_models
 from models.knn.knn import knn
 from models.mlp.mlp_nk import multilayer_perceptron
 from models.random_forest.mdp_random import rf
@@ -20,7 +23,7 @@ buttons_single = []
 buttons_single_texts = ["naive_bayes", "svm", "ADTree", "dt", "lr", "mlp", "adaboost", "xgboost", "random_forest",
                         "knn"]
 buttons_combine = []
-buttons_combine_texts = ["naive_bayes", "svm", "ADTree", "DT", "LR", "mlp", "adaboost", "xgboost", "random_forest",
+buttons_combine_texts = ["naive_bayes", "svm", "ADTree", "dt", "lr", "mlp", "adaboost", "xgboost", "random_forest",
                          "knn"]
 single = 0
 combine = 0
@@ -42,10 +45,11 @@ def button_data_click():
     bug_label = b'buggy'
 
     if soc == 1:
-        if len(models) > 1:
+        if len(models) == 2:
             print(f"组合模型: {models}")
+            combine_models(folder_path, bug_label, models)
         else:
-            print('模型选择需要大于等于2')
+            print('模型选择需要等于2')
     else:
         print(f"单个模型: {single_model}")
         if single_model == 'naive_bayes':
@@ -70,7 +74,7 @@ def button_data_click():
             knn(folder_path, bug_label)
 
     # label_select.config(text="Button Clicked!")
-                                                  
+
 
 # 组合模型预测的模型选择
 def on_checkbox_click():
@@ -109,9 +113,9 @@ def show_models_combine():
         for i in range(len(buttons_combine_texts)):
             check_combines = tk.IntVar()
             buttons_combine.append(check_combines)
-            checkbox = tk.Checkbutton(container_combine_model_show, text=buttons_combine_texts[i],
-                                      variable=check_combines,
-                                      command=on_checkbox_click)
+            checkbox = ttk.Checkbutton(container_combine_model_show, text=buttons_combine_texts[i],
+                                       variable=check_combines,
+                                       command=on_checkbox_click)
             checkbox.grid(row=i // 5, column=i % 5)
         combine = 1
 
@@ -138,6 +142,10 @@ def show_models_single():
 root = tk.Tk()
 root.title('缺陷预测模型')
 root.geometry('450x300')
+style = Style()
+style = Style(theme='superhero')
+# cosmo - flatly - journal - literal - lumen - minty - pulse - sandstone - united - yeti（浅色主题）
+# cyborg - darkly - solar - superhero（深色主题）
 label_title = tk.Label(root, text="缺陷预测模型")
 label_title.pack(side='top')
 
@@ -146,17 +154,17 @@ container_data_select = tk.Frame(root)
 label_data = tk.Label(root, text="数据选择")
 label_data.pack(side=tk.TOP)
 radio_var = tk.StringVar(value="AEEEM")
-radio_button1 = tk.Radiobutton(container_data_select, text="AEEEM", variable=radio_var, value="AEEEM",
-                               command=on_radio_button_click)
-radio_button1.pack(side=tk.LEFT)
-radio_button2 = tk.Radiobutton(container_data_select, text="MORPH", variable=radio_var, value="MORPH",
-                               command=on_radio_button_click)
-radio_button2.pack(side=tk.LEFT)
-radio_button3 = tk.Radiobutton(container_data_select, text="RELINK", variable=radio_var, value="RELINK",
-                               command=on_radio_button_click)
-radio_button3.pack(side=tk.LEFT)
-radio_button4 = tk.Radiobutton(container_data_select, text="SOFTLAB", variable=radio_var, value="SOFTLAB",
-                               command=on_radio_button_click)
+radio_button1 = ttk.Radiobutton(container_data_select, text="AEEEM", variable=radio_var, value="AEEEM",
+                                command=on_radio_button_click)
+radio_button1.pack(side=tk.LEFT, padx=5)
+radio_button2 = ttk.Radiobutton(container_data_select, text="MORPH", variable=radio_var, value="MORPH",
+                                command=on_radio_button_click)
+radio_button2.pack(side=tk.LEFT, padx=5)
+radio_button3 = ttk.Radiobutton(container_data_select, text="RELINK", variable=radio_var, value="RELINK",
+                                command=on_radio_button_click)
+radio_button3.pack(side=tk.LEFT, padx=5)
+radio_button4 = ttk.Radiobutton(container_data_select, text="SOFTLAB", variable=radio_var, value="SOFTLAB",
+                                command=on_radio_button_click)
 radio_button4.pack(side=tk.LEFT)
 container_data_select.pack()
 
